f9095860aa038f391fad922a205e6636ef45fd0f
Sean Oughton <seanoughton@gmail.com>
2018-02-03 01:33:58 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS = [] || BRANDS.size > 0#.include?(!@brand)
+    if BRANDS.include?(@brand)
+      puts ""
+    else 
       BRANDS << @brand
     end
 
