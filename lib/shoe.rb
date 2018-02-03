b312c4f78d533403368b1f44dd9d3f83612e6952
Sean Oughton <seanoughton@gmail.com>
2018-02-03 01:25:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand=(brand)
-    @brand = brand
     BRANDS << @brand
   end
+
 
   def cobble
     self.condition = "new"
